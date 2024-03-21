fbbf91d6bed8c6c1bb6a6fe8777cc29dce601a94
Viktor.Plotnikov <viktor.plotnikov@murka.com>
2024-03-21 10:30:35 +0000

    Change comments in update.sh

--- docker/update.sh
+++ docker/update.sh
@@ -10,15 +10,13 @@ updatet_images=()
 declare -A map
 declare -A service_map
 
-# Заполняем хэш-карту соответствия между именами сервисов и контейнеров
+# Filling out the hash map of correspondence between the names of services and containers
 service_map["trading_gateway"]="trading.production.gateway"
 service_map["trading_billing"]="trading.production.billing"
 service_map["trading_auth"]="trading.production.auth"
 service_map["trading_loyalty"]="trading.production.loyalty"
 service_map["trading_mail"]="trading.production.mail"
 service_map["trading_worker_market"]="trading.production.worker_market"
-
-
 
 
 for repo in $repos; do
@@ -29,24 +27,24 @@ for repo in $repos; do
     if [[ $tag == *"latest"* ]] || [[ $tag == *"v1.0.0"* ]]; then
       continue
     fi
-    # создать отсортированный массив по значению из tag
+    # create a sorted array by value from tag
     tag_array+=($tag)
 
   done
-  # отсортировать массив по значению из tag
+  # sort array by value from tag
   sorted_tag_array=($(printf '%s\n' "${tag_array[@]}" | sort -V))
   echo "sorted_tag_array: ${sorted_tag_array[@]}"
-  # получить последний элемент массива
+  # get the last element of the array
   last_tag=${sorted_tag_array[-1]}
   echo "last_tag: $last_tag"
-  # получить имя репозитория
+  # get repository name
   repo_name=$(echo "$repo" | cut -d'/' -f2)
   echo "repo_name: $repo_name"
 
   sed -i "s/$repo_name.*/$repo_name:$last_tag/g" /home/ubuntu/docker-compose.yml
   updatet_images+=("$repo_name:$last_tag")
 
-  # Находим соответствующий сервис для репозитория
+  # Finding the appropriate service for the repository
   service_name=${service_map[$repo_name]}
   if [ ! -z "$service_name" ]; then
     map+=([$service_name]="$repo_name:$last_tag")
@@ -56,7 +54,7 @@ done
 docker-compose -f /home/ubuntu/docker-compose.yml pull
 docker stack deploy --with-registry-auth -c /home/ubuntu/docker-compose.yml trading
 
-# Обновляем сервисы в Docker Swarm
+# Update services in Docker Swarm
 for service in "${!map[@]}"; do
   echo "service: $service"
   echo "image: ${map[$service]}"
